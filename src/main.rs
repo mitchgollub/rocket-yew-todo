f703aa7874cd261ef2737b68bec2cfc21138e555
mitchgollub <mgollub@live.com>
2020-09-11 20:15:50 -0400

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+
+#[cfg(test)]
+mod tests;
+
+#[get("/")]
+fn hello() -> &'static str {
+    "Hello, world!"
+}
+
+use rocket_contrib::serve::StaticFiles;
+fn main() {
+    rocket::ignite()
+        .mount("/api", routes![hello])
+        .mount("/", StaticFiles::from("./yew-app/static"))
+        .launch();
+}
